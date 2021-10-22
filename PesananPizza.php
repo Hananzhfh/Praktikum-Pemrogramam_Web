02538544f4e8d147f2b8caf87b56e1fc495484a6
Hananzhfh <92773380+Hananzhfh@users.noreply.github.com>
2021-10-22 16:49:25 +0700

    Add files via upload

---
+++ PesananPizza.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bukti Pemesanan</title>
+</head>
+<body style="background-image: linear-gradient(white,rgb(6, 255, 159)); width: auto; height: 640px;">
+    <div style="background-color:pink;margin:auto;max-width: 800px; border: 2px solid black; padding: 10px;">
+        <h1 style="text-align: center;">THANK YOU FOR YOUR ORDER</h1>
+        <hr style="border-color:blue;">
+        <p>Thank you for ordering from Black Goose Bistro. We have received the following information about your order</p>
+        <h3 style="color:red;">Your Information</h3>
+        <table style="margin-left:25px;">
+            <tr>
+                <td><b>Name</b></td>
+                <td>: Hana Yasmin Nazhifah </td>
+            </tr> 
+            <tr>
+                <td><b>Address</b></td>
+                <td>: Bekasi </td>
+            </tr>
+            <tr>
+                <td><b>Telephone Number</b></td>
+                <td>: 021675432190 </td>
+            </tr>
+            <tr>
+                <td><b>Email</b></td>
+                <td>: hananzhfh14@gmail.com</td>
+            </tr>
+        </table>
+                <P><b>Deliveery Instructions:</b> Ring the middle buzzer. If nobody answers, text me.</P>
+            <h3 style="color:red;">Your pizza</h3>
+        <table style="margin-left:25px;">
+            <tr>
+                <td><b>Crust</b></td>
+                <td>: Melt Cheese </td>
+            </tr>
+            <tr>
+            <td><b>Toppings</b></td>
+                <td>: Red sauce, Mozarella, Pepperoni </td>
+            </tr>
+            <tr>
+                <td><b>Number</b></td>
+                <td>: 3 </td>
+            </tr>
+        </table>
+                <hr style="border-color:red;">
+                <p>This site is for educational purposes only. No pizzas will be delivered</p>
+    </div>
+</body>
+</html>
